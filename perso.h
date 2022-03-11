223ac954c9a3af3908a7eac251cf2595abb73321
ARnidhal <nidhal.arfaoui@esprit.tn>
2022-03-11 11:05:20 +0100

    maj

---
+++ perso.h
@@ -0,0 +1,27 @@
+#ifndef PERSONNE_H_INCLUDE
+#define PERSONNE_H_INCLUDE
+     typedef struct
+     {
+        SDL_Surface *perso;
+        SDL_Rect pos;
+        int score;
+        int vies;
+     }personne;
+     void intperso(personne*p)
+     {
+
+     }
+     void afficheperso(personne p,SDL_Surface *screen)
+     {
+
+     }
+     void deplacerperso(personne*p,int dt){
+
+     }
+     void animerperso (personne *p){
+
+     }
+void saut(personne *p)
+{
+
+}
